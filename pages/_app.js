94663fea1aec255dfc58db6c9dfcaa37494f5198
DeadpoolSteinS <novaldisago@gmail.com>
2022-11-06 22:33:57 +0700

    delete templatemo style

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import "../styles/globals.css";
 import "../styles/unicons.css";
 // import "../styles/bootstrap.css";
-// import "../styles/templatemo.css";
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
