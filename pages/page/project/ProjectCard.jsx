7a8ad8fb7bc2eb67c489f964e730988149b44ff0
DeadpoolSteinS <novaldisago@gmail.com>
2022-11-09 00:13:33 +0700

    data with '?' for null check

--- pages/page/project/ProjectCard.jsx
+++ pages/page/project/ProjectCard.jsx
@@ -5,15 +5,15 @@ const ProjectCard = (props) => {
     <a href="#" className="relative">
       <Image
         className="rounded"
-        src="https://raw.githubusercontent.com/DeadpoolSteinS/sejuta-cita-frontend/master/img/pc-version.jpg"
+        src={props.data?.image}
         alt=""
         width="400"
         height="300"
       />
       <div className="absolute w-full rounded top-0 bottom-0 p-4 bg-portfolio-gradient bg-size-300 bg-position-100 hover:bg-position-0 text-white hover:text-black flex items-end transition-all duration-500">
         <div>
-          <p className="font-bold">{props.data.title}</p>
-          <p className="line-clamp-2">{props.data.desc}</p>
+          <p className="font-bold">{props.data?.title}</p>
+          <p className="line-clamp-2">{props.data?.desc}</p>
         </div>
       </div>
     </a>
