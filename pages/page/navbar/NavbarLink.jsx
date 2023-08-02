5322b9a5b3caeacb3848fa98d07c64d58051e6cf
NovaldiS Ago <75278889+DeadpoolSteinS@users.noreply.github.com>
2023-08-02 12:34:51 +0700

    memperbaiki error undefined tolowercase part 2

--- pages/page/navbar/NavbarLink.jsx
+++ pages/page/navbar/NavbarLink.jsx
@@ -1,18 +1,20 @@
 const NavbarLink = ({name}) => {
   return (
-    <li>
-      <a
-        href={`#${name.toLowerCase()}`}
-        className="font-bold opacity-50 block px-5 overflow-hidden [&:hover>span]:-translate-y-full [&:hover>span]:text-[#ffc200]"
-      >
+    name && (
+      <li>
+        <a
+          href={`#${name.toLowerCase()}`}
+          className="font-bold opacity-50 block px-5 overflow-hidden [&:hover>span]:-translate-y-full [&:hover>span]:text-[#ffc200]"
+        >
         <span
           data-hover={name}
           className="transition-transform inline-block relative before:content-[attr(data-hover)] before:absolute before:top-full"
         >
           {name}
         </span>
-      </a>
-    </li>
+        </a>
+      </li>
+    )
   );
 };
 
