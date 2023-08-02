bf54a01149c01e89fcc02c60be03e3c90be25b8b
NovaldiS Ago <75278889+DeadpoolSteinS@users.noreply.github.com>
2023-08-02 12:33:07 +0700

    memperbaiki error undefined part 1

--- pages/page/navbar/NavbarLink.jsx
+++ pages/page/navbar/NavbarLink.jsx
@@ -1,15 +1,15 @@
-const NavbarLink = (props) => {
+const NavbarLink = ({name}) => {
   return (
     <li>
       <a
-        href={`#${props.name.toLowerCase()}`}
+        href={`#${name.toLowerCase()}`}
         className="font-bold opacity-50 block px-5 overflow-hidden [&:hover>span]:-translate-y-full [&:hover>span]:text-[#ffc200]"
       >
         <span
-          data-hover={props.name}
+          data-hover={name}
           className="transition-transform inline-block relative before:content-[attr(data-hover)] before:absolute before:top-full"
         >
-          {props.name}
+          {name}
         </span>
       </a>
     </li>
